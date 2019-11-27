bd45adf774a93f6357dd83e203f2affd07c5c240
Julia <juliawang105@gmail.com>
2019-11-27 10:58:42 -0800

    render circles according to view counts

--- src/bubbles.js
+++ src/bubbles.js
@@ -1,41 +1,43 @@
 export const bubbles = () => {
    console.log('bubbles is working')
-   // let url = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=GTmpcsZu5C6PWkRN8a3pHDaLwB8kCULG";
+     
+   //let url = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=GTmpcsZu5C6PWkRN8a3pHDaLwB8kCULG";
 
-   // d3.json(url).then(
-   //    data => {
-   //       let arr = data.results.reverse()
-   //       console.log(arr)
-   //       let content = document.getElementById("content");
-   //       for (let i = 0; i < 10; i++) {
-   //          let article = document.createElement('p')
-   //          article.innerHTML = arr[i].title;
-   //          content.appendChild(article)
-   //       }
-   //    }
-   // )
-      
-   // let circleData = [
-   //    {"cx:": 20, "cy": 20, "radius": 20, "color": "green"},
-   //    { "cx": 70, "cy": 70, "radius": 20, "color": "purple" }];
+   d3.json(url).then(
+      data => {
+         let arr = data.results.reverse()
+         console.log(arr)
+         let content = document.getElementById("content");
+            let size = [];
+         for (let i = 0; i < 10; i++) {
+            let article = document.createElement('p')
+            article.innerHTML = arr[i].title;
+            content.appendChild(article);
+               size.push(arr[i].views)
+         }
+
+         
+
+   // let margin = { left: 100, right: 80, top: 10, bottom: 130 };
+   // let width = 1300 - margin.left - margin.right;
+   // let height = 700 - margin.top - margin.bottom;
+
+   // let chart = d3
+   //         .select("#content").append("svg")
+   //         .attr("height", height + margin.top + margin.bottom)
+   //         .attr("width", width + margin.left + margin.right)
+   //         .style("border", "1px solid black")
+
+   // document.getElementById("content").align = 'center';
+         let circle = d3.selectAll("circle")
+         circle.style("fill", "steelblue")
+         circle.attr("r", 30)
+         console.log(size)
+         // circle.attr("cx", function () {return Math.random() * 720});
+         circle.data(size)
+         circle.attr("r", function (d) { return d})
+   
+   })
 
    
-   let svgContainer = d3.selectAll('.name').style("color", "green").append('svg')
-                                                                   .attr('width', 200)
-                                                                   .attr("height",200)
-                                                                  //  .attr('border')
-
-   
-   
-  
-   let circle = svgContainer.append('circle')
-                            .attr('cx', 30)
-                            .attr('cy', 30)
-                            .attr('r', 70)
-                            .append("p")
-
-   
-
-
-
-};
+}
